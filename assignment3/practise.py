a14f9face35d6bb72c45a3001d049cddfb3eda0c
ชลธิดา โพธิ์เงิน <s5902041630038@Macmini-213-29.local>
2019-07-11 16:52:38 +0700

    add files for assignment3

---
+++ assignment3/practise.py
@@ -0,0 +1,10 @@
+my_list =[50, 28, 22]
+print(sum(my_list))
+
+my_tuble = (5, 10, 15, 20, 25)
+print(my_tuble)
+
+set1 = {2, 5, 7, 9, 10, 12, 16, 40, 60, 77, 99}
+set2 = {1, 5, 9, 12, 15, 20, 30, 77, 80, 90, 95}
+print(set1.intersection(set2))
+
