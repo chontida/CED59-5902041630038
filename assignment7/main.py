949779cff860f7f5a1c9ac10dddddde9cec9b37d
Computer Education <computereducation@Macmini-213-48.local>
2019-09-05 13:54:15 +0700

    add files for assignment8

--- assignment7/main.py
+++ assignment7/main.py
@@ -1,9 +1,22 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
 
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
 @app.route("/")
-def member():
+def redirect():
     return render_template('login.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
+
+if __name__ == '__main__':
+    app.run()
